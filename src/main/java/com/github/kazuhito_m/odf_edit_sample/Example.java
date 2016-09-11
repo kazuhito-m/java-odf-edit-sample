fbf4ddd5b7fa03ea6bf6728e1a801cb71d5b6f21
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-09-12 03:25:56 +0900

    ページテンプレートを使ったページ遷移に変更。

--- src/main/java/com/github/kazuhito_m/odf_edit_sample/Example.java
+++ src/main/java/com/github/kazuhito_m/odf_edit_sample/Example.java
@@ -2,29 +2,31 @@ package com.github.kazuhito_m.odf_edit_sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * 最小限のサンプルです。
  */
-@RestController
+@Controller
 @EnableAutoConfiguration
 public class Example {
-    /**
-     * 「Hello World!」と表示します。
-     * @return メッセージ
-     */
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
-    }
+
     /**
      * 処理を実行します。
+     *
      * @param args 引数
      * @throws Exception 例外が発生した場合
      */
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Example.class, args);
+    }
+
+    @RequestMapping({"/", "/hello"})
+    public String hello(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
     }
 }
