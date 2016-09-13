44cfaba28697152c845e1c0e76c38fc24a7f77a4
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-09-14 02:22:02 +0900

    Flywayマイグレーション追加。テーブル作成とイニシャルデータ。

---
+++ src/main/resources/db/migration/V0001__create_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE user (
+    id MEDIUMINT  NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    caption VARCHAR(255),
+    mailAddress VARCHAR(255),
+    description VARCHAR(255),
+    password VARCHAR(255),
+    PRIMARY KEY (ID)
+);
---
+++ src/main/resources/db/migration/V0002__insert_initial_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO user (name,caption,mailAddress,description,password) VALUES ('Kazuhito Miura','三浦 一仁','kazuhito.miura@example.com','唯一のユーザ','pswd');
+INSERT INTO user (name,caption,mailAddress,description,password) VALUES ('Jenkins','Jenkinsの爺さん(執事)','jenkins@example.com','管理用ユーザ','pswd');
