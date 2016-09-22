a7264dc6c3cf7678bb59ee160bc7775ac5753319
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-09-23 04:34:28 +0900

    テストデータ追加。

--- src/main/resources/db/migration/V0001__create_table.sql
+++ src/main/resources/db/migration/V0001__create_table.sql
@@ -7,3 +7,14 @@ CREATE TABLE user (
     password VARCHAR(255),
     PRIMARY KEY (ID)
 );
+
+CREATE TABLE workresult_day (
+    id MEDIUMINT NOT NULL AUTO_INCREMENT,
+    user_id NOT NULL
+    result_date DATE NOT NULL ,
+    start_time TIME,
+    end_time TIME,
+    break_hour FLOAT,
+    description VARCHAR(255),
+    PRIMARY KEY (ID)
+);
