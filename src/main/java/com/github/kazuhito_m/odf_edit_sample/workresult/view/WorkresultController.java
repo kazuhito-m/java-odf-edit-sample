ea88c53213941e2ac4463214ba1339dde362b614
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-09-23 22:46:57 +0900

    実装。とりあえず「ファイルのダウンロードが出来る機構」だけを作成。ダミーでおとさせる。

--- src/main/java/com/github/kazuhito_m/odf_edit_sample/workresult/view/WorkresultController.java
+++ src/main/java/com/github/kazuhito_m/odf_edit_sample/workresult/view/WorkresultController.java
@@ -1,17 +1,26 @@
 package com.github.kazuhito_m.odf_edit_sample.workresult.view;
 
 import com.github.kazuhito_m.odf_edit_sample.workresult.domain.Workresult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Collections;
 import java.util.List;
 
 @Controller
 public class WorkresultController {
+
+    private static final Logger logger = LoggerFactory.getLogger(WorkresultController.class);
 
     @Autowired
     private Workresult domain;
@@ -32,6 +41,20 @@ public class WorkresultController {
         model.addAttribute("workresults", workresults);
 
         return "workresult";
+    }
+
+    @RequestMapping({"/dlworkresult"})
+    public ResponseEntity<byte[]> dlWorkresult(@ModelAttribute("form") ConditionForm form, Model model) throws UnsupportedEncodingException {
+
+        if (StringUtils.isEmpty(form.getMonth())) {
+            form.setMonth(domain.getNowMonth());
+        }
+
+        HttpHeaders h = new HttpHeaders();
+        h.add("Content-Type", "text/csv; charset=MS932");
+        h.setContentDispositionFormData("filename", "hoge.csv");
+        return new ResponseEntity<>(form.getMonth().getBytes("UTF-8"), h, HttpStatus.OK);
+
     }
 
 }
