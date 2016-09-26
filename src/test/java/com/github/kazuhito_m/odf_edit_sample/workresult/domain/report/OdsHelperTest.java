d2477c3664fb04d79627cdaf6a2a4bc225918615
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-09-26 21:50:27 +0900

    バグフィックス。結果の変わる不安定なテストを修正。

--- src/test/java/com/github/kazuhito_m/odf_edit_sample/workresult/domain/report/OdsHelperTest.java
+++ src/test/java/com/github/kazuhito_m/odf_edit_sample/workresult/domain/report/OdsHelperTest.java
@@ -1,14 +1,14 @@
 package com.github.kazuhito_m.odf_edit_sample.workresult.domain.report;
 
-
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class OdsHelperTest {
@@ -24,12 +24,12 @@ public class OdsHelperTest {
 
     @Test
     public void JavaのDateの内部保持値をODSをの内部保持値に変換を試みる() {
-        assertThat(sut.convJavaDateToExcelDateValue(1800000L), is(bd("0.39583333333333319400")));
-        assertThat(sut.convJavaDateToExcelDateValue(16200000L), is(bd("0.56249999999999980200")));
+        assertThat(to10p(sut.convJavaDateToExcelDateValue(1800000L)), is("0.3958333333"));
+        assertThat(to10p(sut.convJavaDateToExcelDateValue(16200000L)), is("0.5624999999"));
     }
 
-    private BigDecimal bd(String value) {
-        return new BigDecimal(value);
+    private String to10p(double value) {
+        return BigDecimal.valueOf(value).setScale(10, RoundingMode.DOWN).toString();
     }
 
 }
