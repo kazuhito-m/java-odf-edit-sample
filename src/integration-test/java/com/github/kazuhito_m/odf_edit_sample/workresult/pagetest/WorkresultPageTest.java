4f82feccb77bef55b78b0609cbb9dee8229eaa5f
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-12-09 03:26:03 +0900

    Firefox対策。Selenideに「自作のFirefoxDriverを設定」する。

--- src/integration-test/java/com/github/kazuhito_m/odf_edit_sample/workresult/pagetest/WorkresultPageTest.java
+++ src/integration-test/java/com/github/kazuhito_m/odf_edit_sample/workresult/pagetest/WorkresultPageTest.java
@@ -1,15 +1,26 @@
 package com.github.kazuhito_m.odf_edit_sample.workresult.pagetest;
 
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.concurrent.TimeUnit;
+import static com.codeborne.selenide.Selenide.close;
+import static com.codeborne.selenide.Selenide.open;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class WorkresultPageTest {
 
     private static final Logger logger = LoggerFactory.getLogger(WorkresultPageTest.class);
+
+    @Before
+    public void setUp() {
+        // Selenideに「自作のFirefoxドライバー」をセット。
+        WebDriverRunner.setWebDriver(FirefoxWebDriverFactroy.create());
+    }
 
     @Test
     public void 初期表示を確認() {
@@ -17,19 +28,16 @@ public class WorkresultPageTest {
         logger.debug("IntegrationiTestのこのメソッドが通ってるか確認 : 初期表示を確認()");
 
 
-        WebDriver driver = FirefoxWebDriverFactroy.create(); //上記Driver.java記載の処理
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.get("http://localhost:8080/"); // Basic認証が必要なサイトへリクエスト送信する
-        driver.manage().window().maximize();
-        driver.quit();
+        open("http://localhost:8080/");
 
+        // 検証
+        assertThat("同じ", is("同じ"));
 
-//        open("http://localhost:8080/");
-//
-//        // 検証
-//        assertThat("同じ", is("同じ"));
-//
-//        close();
+    }
+
+    @After
+    public void tearDown() {
+        close();
     }
 
 }
